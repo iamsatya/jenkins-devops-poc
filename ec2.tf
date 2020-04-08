a4df25f0ffdead88e426ad39b2ba8602f51d297d
satya <satyapanuganti@gmail.com>
2020-04-08 13:23:00 +0530

    changed image d

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "instance" {
   instance_type = "t2.micro"
-  image_id      = "ami-03b5297d565ef30a6"
+  ami           = "ami-03b5297d565ef30a6"
 
   tags = {
     "type" = "terraform-test-instance"
