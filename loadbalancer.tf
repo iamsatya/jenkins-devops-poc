50ce4af515fa59bd8765fb751efdaeb3311002ca
satya <satyapanuganti@gmail.com>
2020-04-09 23:25:34 +0530

    changed 8080 to 80

--- loadbalancer.tf
+++ loadbalancer.tf
@@ -59,11 +59,11 @@ resource "aws_lb_target_group"  "apptg" {
 resource "aws_lb_target_group_attachment" "app1tg" {
   target_group_arn = aws_lb_target_group.apptg.arn
   target_id        = aws_instance.app1.id
-  port             = 8080
+  port             = 80
 }
 
 resource "aws_lb_target_group_attachment" "app2tg" {
   target_group_arn = aws_lb_target_group.apptg.arn
   target_id        = aws_instance.app2.id
-  port             = 8080
+  port             = 80
 }
