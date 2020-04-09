65d95361efad9b7b4fe6164a9bdcdf5e9a7ed2b4
satya <satyapanuganti@gmail.com>
2020-04-09 13:04:20 +0530

    added elearning app

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -1,346 +1,262 @@
- AOS.init({
- 	duration: 800,
- 	easing: 'slide'
- });
+$(function() {
+	'use strict';
 
-(function($) {
+	var window_width = $(window).width(),
+		window_height = window.innerHeight,
+		header_height = $('.default-header').height(),
+		header_height_static = $('.site-header.static').outerHeight(),
+		fitscreen = window_height - header_height;
 
-	"use strict";
+	$('.fullscreen').css('height', window_height);
+	$('.fitscreen').css('height', fitscreen);
 
-	var isMobile = {
-		Android: function() {
-			return navigator.userAgent.match(/Android/i);
-		},
-			BlackBerry: function() {
-			return navigator.userAgent.match(/BlackBerry/i);
-		},
-			iOS: function() {
-			return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-		},
-			Opera: function() {
-			return navigator.userAgent.match(/Opera Mini/i);
-		},
-			Windows: function() {
-			return navigator.userAgent.match(/IEMobile/i);
-		},
-			any: function() {
-			return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+	//------- Header Scroll Class  js --------//
+
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > 100) {
+			$('.default-header').addClass('header-scrolled');
+		} else {
+			$('.default-header').removeClass('header-scrolled');
 		}
-	};
-
-
-	$(window).stellar({
-    responsive: true,
-    parallaxBackgrounds: true,
-    parallaxElements: true,
-    horizontalScrolling: false,
-    hideDistantElements: false,
-    scrollProperty: 'scroll'
-  });
-
-
-	var fullHeight = function() {
-
-		$('.js-fullheight').css('height', $(window).height());
-		$(window).resize(function(){
-			$('.js-fullheight').css('height', $(window).height());
-		});
-
-	};
-	fullHeight();
-
-	// loader
-	var loader = function() {
-		setTimeout(function() { 
-			if($('#ftco-loader').length > 0) {
-				$('#ftco-loader').removeClass('show');
-			}
-		}, 1);
-	};
-	loader();
-
-	// Scrollax
-   $.Scrollax();
-
-	var carousel = function() {
-		$('.home-slider').owlCarousel({
-	    loop:true,
-	    autoplay: true,
-	    margin:0,
-	    animateOut: 'fadeOut',
-	    animateIn: 'fadeIn',
-	    nav:false,
-	    autoplayHoverPause: false,
-	    items: 1,
-	    navText : ["<span class='ion-md-arrow-back'></span>","<span class='ion-chevron-right'></span>"],
-	    responsive:{
-	      0:{
-	        items:1
-	      },
-	      600:{
-	        items:1
-	      },
-	      1000:{
-	        items:1
-	      }
-	    }
-		});
-	
-		$('.carousel-testimony').owlCarousel({
-			center: true,
-			loop: true,
-			items:1,
-			margin: 30,
-			stagePadding: 0,
-			nav: false,
-			navText: ['<span class="ion-ios-arrow-back">', '<span class="ion-ios-arrow-forward">'],
-			responsive:{
-				0:{
-					items: 1
-				},
-				600:{
-					items: 1
-				},
-				1000:{
-					items: 1
-				}
-			}
-		});
-
-	};
-	carousel();
-
-	$('nav .dropdown').hover(function(){
-		var $this = $(this);
-		// 	 timer;
-		// clearTimeout(timer);
-		$this.addClass('show');
-		$this.find('> a').attr('aria-expanded', true);
-		// $this.find('.dropdown-menu').addClass('animated-fast fadeInUp show');
-		$this.find('.dropdown-menu').addClass('show');
-	}, function(){
-		var $this = $(this);
-			// timer;
-		// timer = setTimeout(function(){
-			$this.removeClass('show');
-			$this.find('> a').attr('aria-expanded', false);
-			// $this.find('.dropdown-menu').removeClass('animated-fast fadeInUp show');
-			$this.find('.dropdown-menu').removeClass('show');
-		// }, 100);
 	});
 
-
-	$('#dropdown04').on('show.bs.dropdown', function () {
-	  console.log('show');
-	});
-
-	// scroll
-	var scrollWindow = function() {
-		$(window).scroll(function(){
-			var $w = $(this),
-					st = $w.scrollTop(),
-					navbar = $('.ftco_navbar'),
-					sd = $('.js-scroll-wrap');
-
-			if (st > 150) {
-				if ( !navbar.hasClass('scrolled') ) {
-					navbar.addClass('scrolled');	
-				}
-			} 
-			if (st < 150) {
-				if ( navbar.hasClass('scrolled') ) {
-					navbar.removeClass('scrolled sleep');
-				}
-			} 
-			if ( st > 350 ) {
-				if ( !navbar.hasClass('awake') ) {
-					navbar.addClass('awake');	
-				}
-				
-				if(sd.length > 0) {
-					sd.addClass('sleep');
-				}
-			}
-			if ( st < 350 ) {
-				if ( navbar.hasClass('awake') ) {
-					navbar.removeClass('awake');
-					navbar.addClass('sleep');
-				}
-				if(sd.length > 0) {
-					sd.removeClass('sleep');
-				}
-			}
-		});
-	};
-	scrollWindow();
-
-	
-	var counter = function() {
-		
-		$('#section-counter').waypoint( function( direction ) {
-
-			if( direction === 'down' && !$(this.element).hasClass('ftco-animated') ) {
-
-				var comma_separator_number_step = $.animateNumber.numberStepFactories.separator(',')
-				$('.number').each(function(){
-					var $this = $(this),
-						num = $this.data('number');
-						console.log(num);
-					$this.animateNumber(
-					  {
-					    number: num,
-					    numberStep: comma_separator_number_step
-					  }, 7000
-					);
-				});
-				
-			}
-
-		} , { offset: '95%' } );
-
+	if ($('select')) {
+		$('select').niceSelect();
 	}
-	counter();
 
-	var contentWayPoint = function() {
-		var i = 0;
-		$('.ftco-animate').waypoint( function( direction ) {
+	$('.img-pop-up').magnificPopup({
+		type: 'image',
+		gallery: {
+			enabled: true
+		}
+	});
 
-			if( direction === 'down' && !$(this.element).hasClass('ftco-animated') ) {
-				
-				i++;
+	// Search Toggle
+	$('#search-input-box').hide();
+	$('#search').on('click', function() {
+		$('#search-input-box').slideToggle();
+		$('#search-input').focus();
+	});
+	$('#close-search').on('click', function() {
+		$('#search-input-box').slideUp(500);
+	});
 
-				$(this.element).addClass('item-animate');
-				setTimeout(function(){
+	// $('.navbar-nav>li>a').on('click', function(){
+	//     $('.navbar-collapse').collapse('hide');
+	// });
 
-					$('body .ftco-animate.item-animate').each(function(k){
-						var el = $(this);
-						setTimeout( function () {
-							var effect = el.data('animate-effect');
-							if ( effect === 'fadeIn') {
-								el.addClass('fadeIn ftco-animated');
-							} else if ( effect === 'fadeInLeft') {
-								el.addClass('fadeInLeft ftco-animated');
-							} else if ( effect === 'fadeInRight') {
-								el.addClass('fadeInRight ftco-animated');
-							} else {
-								el.addClass('fadeInUp ftco-animated');
-							}
-							el.removeClass('item-animate');
-						},  k * 50, 'easeInOutExpo' );
-					});
-					
-				}, 100);
-				
+	//  Counter Js
+
+	$('.counter').counterUp({
+		delay: 10,
+		time: 1000
+	});
+
+	$('.play-btn').magnificPopup({
+		type: 'iframe',
+		mainClass: 'mfp-fade',
+		removalDelay: 160,
+		preloader: false,
+		fixedContentPos: false
+	});
+
+	$('.popuar-course-carusel').owlCarousel({
+		items: 4,
+		loop: true,
+		autoplay: true,
+		margin: 30,
+		nav: true,
+		stagePadding: 60,
+		navText: [ "<img src='img/prev.png'>", "<img src='img/next.png'>" ],
+		responsive: {
+			0: {
+				items: 1,
+				stagePadding: 0
+			},
+			575: {
+				items: 2,
+				stagePadding: 0
+			},
+			768: {
+				items: 2,
+				stagePadding: 0
+			},
+			992: {
+				items: 3,
+				stagePadding: 0
+			},
+			1200: {
+				items: 3,
+				stagePadding: 60
+			},
+			1440: {
+				items: 4,
+				stagePadding: 60
 			}
+		}
+	});
 
-		} , { offset: '95%' } );
-	};
-	contentWayPoint();
+	$('.video-carousel').owlCarousel({
+		items: 1,
+		loop: true,
+		autoplay: true,
+		margin: 30,
+		nav: true,
+		dots: false,
+		navText: [ "<img src='img/prev.png'>", "<img src='img/next.png'>" ]
+	});
 
+	$('.testi-slider').owlCarousel({
+		items: 1,
+		loop: true,
+		autoplay: true,
+		margin: 30,
+		nav: true,
+		navText: [ "<img src='img/prev.png'>", "<img src='img/next.png'>" ]
+	});
 
-	// navigation
-	var OnePageNav = function() {
-		$(".smoothscroll[href^='#'], #ftco-nav ul li a[href^='#']").on('click', function(e) {
-		 	e.preventDefault();
-
-		 	var hash = this.hash,
-		 			navToggler = $('.navbar-toggler');
-		 	$('html, body').animate({
-		    scrollTop: $(hash).offset().top
-		  }, 700, 'easeInOutExpo', function(){
-		    window.location.hash = hash;
-		  });
-
-
-		  if ( navToggler.is(':visible') ) {
-		  	navToggler.click();
-		  }
+	// Select all links with hashes
+	$('.navbar-nav a[href*="#"]')
+		// Remove links that don't actually link to anything
+		.not('[href="#"]')
+		.not('[href="#0"]')
+		.on('click', function(event) {
+			// On-page links
+			if (
+				location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+				location.hostname == this.hostname
+			) {
+				// Figure out element to scroll to
+				var target = $(this.hash);
+				target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+				// Does a scroll target exist?
+				if (target.length) {
+					// Only prevent default if animation is actually gonna happen
+					event.preventDefault();
+					$('html, body').animate(
+						{
+							scrollTop: target.offset().top - 50
+						},
+						1000,
+						function() {
+							// Callback after animation
+							// Must change focus!
+							var $target = $(target);
+							$target.focus();
+							if ($target.is(':focus')) {
+								// Checking if the target was focused
+								return false;
+							} else {
+								$target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+								$target.focus(); // Set focus again
+							}
+						}
+					);
+				}
+			}
 		});
-		$('body').on('activate.bs.scrollspy', function () {
-		  console.log('nice');
-		})
-	};
-	OnePageNav();
 
+	// Google Map
+	if (document.getElementById('map')) {
+		google.maps.event.addDomListener(window, 'load', init);
 
-	// magnific popup
-	$('.image-popup').magnificPopup({
-    type: 'image',
-    closeOnContentClick: true,
-    closeBtnInside: false,
-    fixedContentPos: true,
-    mainClass: 'mfp-no-margins mfp-with-zoom', // class to remove default margin from left and right side
-     gallery: {
-      enabled: true,
-      navigateByImgClick: true,
-      preload: [0,1] // Will preload 0 - before current, and 1 after the current image
-    },
-    image: {
-      verticalFit: true
-    },
-    zoom: {
-      enabled: true,
-      duration: 300 // don't foget to change the duration also in CSS
-    }
-  });
+		function init() {
+			// Basic options for a simple Google Map
+			// For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
+			var mapOptions = {
+				// How zoomed in you want the map to start at (always required)
+				zoom: 11,
 
-  $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
-    disableOn: 700,
-    type: 'iframe',
-    mainClass: 'mfp-fade',
-    removalDelay: 160,
-    preloader: false,
+				// The latitude and longitude to center the map (always required)
+				center: new google.maps.LatLng(40.67, -73.94), // New York
 
-    fixedContentPos: false
-  });
+				// How you would like to style the map.
+				// This is where you would paste any style found on Snazzy Maps.
+				styles: [
+					{
+						featureType: 'water',
+						elementType: 'geometry',
+						stylers: [ { color: '#e9e9e9' }, { lightness: 17 } ]
+					},
+					{
+						featureType: 'landscape',
+						elementType: 'geometry',
+						stylers: [ { color: '#f5f5f5' }, { lightness: 20 } ]
+					},
+					{
+						featureType: 'road.highway',
+						elementType: 'geometry.fill',
+						stylers: [ { color: '#ffffff' }, { lightness: 17 } ]
+					},
+					{
+						featureType: 'road.highway',
+						elementType: 'geometry.stroke',
+						stylers: [ { color: '#ffffff' }, { lightness: 29 }, { weight: 0.2 } ]
+					},
+					{
+						featureType: 'road.arterial',
+						elementType: 'geometry',
+						stylers: [ { color: '#ffffff' }, { lightness: 18 } ]
+					},
+					{
+						featureType: 'road.local',
+						elementType: 'geometry',
+						stylers: [ { color: '#ffffff' }, { lightness: 16 } ]
+					},
+					{
+						featureType: 'poi',
+						elementType: 'geometry',
+						stylers: [ { color: '#f5f5f5' }, { lightness: 21 } ]
+					},
+					{
+						featureType: 'poi.park',
+						elementType: 'geometry',
+						stylers: [ { color: '#dedede' }, { lightness: 21 } ]
+					},
+					{
+						elementType: 'labels.text.stroke',
+						stylers: [ { visibility: 'on' }, { color: '#ffffff' }, { lightness: 16 } ]
+					},
+					{
+						elementType: 'labels.text.fill',
+						stylers: [ { saturation: 36 }, { color: '#333333' }, { lightness: 40 } ]
+					},
+					{ elementType: 'labels.icon', stylers: [ { visibility: 'off' } ] },
+					{
+						featureType: 'transit',
+						elementType: 'geometry',
+						stylers: [ { color: '#f2f2f2' }, { lightness: 19 } ]
+					},
+					{
+						featureType: 'administrative',
+						elementType: 'geometry.fill',
+						stylers: [ { color: '#fefefe' }, { lightness: 20 } ]
+					},
+					{
+						featureType: 'administrative',
+						elementType: 'geometry.stroke',
+						stylers: [ { color: '#fefefe' }, { lightness: 17 }, { weight: 1.2 } ]
+					}
+				]
+			};
 
+			// Get the HTML DOM element that will contain your map
+			// We are using a div with id="map" seen below in the <body>
+			var mapElement = document.getElementById('map');
 
+			// Create the Google Map using our element and options defined above
+			var map = new google.maps.Map(mapElement, mapOptions);
 
-	var goHere = function() {
+			// Let's also add a marker while we're at it
+			var marker = new google.maps.Marker({
+				position: new google.maps.LatLng(40.67, -73.94),
+				map: map,
+				title: 'Snazzy!'
+			});
+		}
+	}
 
-		$('.mouse-icon').on('click', function(event){
-			
-			event.preventDefault();
-
-			$('html,body').animate({
-				scrollTop: $('.goto-here').offset().top
-			}, 500, 'easeInOutExpo');
-			
-			return false;
-		});
-	};
-	goHere();
-
-	function makeTimer() {
-
-		var endTime = new Date("21 December 2019 9:56:00 GMT+01:00");			
-		endTime = (Date.parse(endTime) / 1000);
-
-		var now = new Date();
-		now = (Date.parse(now) / 1000);
-
-		var timeLeft = endTime - now;
-
-		var days = Math.floor(timeLeft / 86400); 
-		var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
-		var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600 )) / 60);
-		var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
-
-		if (hours < "10") { hours = "0" + hours; }
-		if (minutes < "10") { minutes = "0" + minutes; }
-		if (seconds < "10") { seconds = "0" + seconds; }
-
-		$("#days").html(days + "<span>Days</span>");
-		$("#hours").html(hours + "<span>Hours</span>");
-		$("#minutes").html(minutes + "<span>Minutes</span>");
-		$("#seconds").html(seconds + "<span>Seconds</span>");		
-
-}
-
-setInterval(function() { makeTimer(); }, 1000);
-
-
-
-
-})(jQuery);
-
+	$(document).ready(function() {
+		$('#mc_embed_signup').find('form').ajaxChimp();
+	});
+});
