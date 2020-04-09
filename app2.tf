db88b685e7f48395fa04518e86690b59a3ea61a1
satya <satyapanuganti@gmail.com>
2020-04-09 22:21:23 +0530

    removed lb dns output

--- app2.tf
+++ app2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "app2" {
   instance_type = "t2.micro"
   ami           = "ami-03b5297d565ef30a6"
   key_name      = "aws"
-  vpc_security_group_ids = aws_security_group.sg.id
+  security_groups = aws_security_group.sg.id
 
   tags = {
     "type" = "terraform-test-instance"
