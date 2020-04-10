e7a24d484c5e525ff738a344796a6e4a02d325fc
satya <satyapanuganti@gmail.com>
2020-04-10 15:39:12 +0530

    added route53 data field

--- route53.tf
+++ route53.tf
@@ -1,6 +1,10 @@
+data "aws_route53_zone" "cloud" {
+  name         = "cloudlinuxacademy.com."
+}
+
 resource "aws_route53_record" "www" {
-  zone_id = "${aws_route53_zone.primary.zone_id}"
-  name    = "www.cloudlinuxacademy.com"
+  zone_id = "${data.aws_route53_zone.cloud.zone_id}"
+  name    = "www.${data.aws_route53_zone.cloud.name}"
   type    = "CNAME"
   ttl     = "60"
   records = ["${aws_lb.applb.dns_name}"]
