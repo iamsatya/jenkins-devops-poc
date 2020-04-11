088c224c0a95ed6c98256abba1b470b80f9dac17
satya <satyapanuganti@gmail.com>
2020-04-11 22:37:09 +0530

    added modules for cross region deployments

--- CloudWatch.tf
+++ CloudWatch.tf
@@ -41,40 +41,6 @@ resource "aws_cloudwatch_metric_alarm" "app1health" {
   }
 }
 
-resource "aws_route53_health_check" "cla-hc" {
-  fqdn              = "www.cloudlinuxacademy.com"
-  port              = 80
-  type              = "HTTP"
-  resource_path     = "/"
-  failure_threshold = "3"
-  request_interval  = "30"
-
-  tags = {
-    "Name" = "cla-hc"
-  }
-}
-
-resource "aws_cloudwatch_metric_alarm" "cla-hc" {
-  alarm_name          = "cla_healthcheck_failed"
-  namespace           = "AWS/Route53"
-  metric_name         = "HealthCheckStatus"
-  comparison_operator = "LessThanThreshold"
-  evaluation_periods  = "2"
-  period              = "60"
-  statistic           = "Average"
-  threshold           = "1"
-  unit                = "None"
-
-  dimensions = {
-    HealthCheckId = aws_route53_health_check.cla-hc.id
-  }
-
-  alarm_description         = "This metric monitors cla.com whether the service endpoint is down or not."
-  #alarm_actions             = [ "${aws_sns_topic.devops.arn}" ]
-  #insufficient_data_actions = [ "${aws_sns_topic.devops.arn}" ]
-  treat_missing_data        = "breaching"
-}
-
 resource "aws_cloudwatch_metric_alarm" "app2cpu" {
   alarm_name                = "app2-cpu-alarm"
   comparison_operator       = "GreaterThanOrEqualToThreshold"
