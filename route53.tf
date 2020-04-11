886ac4a7a6b191c0f7528f1f84c765102aa360bd
satya <satyapanuganti@gmail.com>
2020-04-11 15:09:24 +0530

    added route53 health check

--- route53.tf
+++ route53.tf
@@ -9,3 +9,32 @@ resource "aws_route53_record" "www" {
   ttl     = "60"
   records = ["${aws_lb.applb.dns_name}"]
 }
+
+resource "aws_sns_topic" "devops" {
+  name = "devops"
+  display_name = "devops"
+}
+
+resource "aws_sns_topic_subscription" "devops-team" {
+  topic_arn = aws_sns_topic.devops.arn
+  protocol  = "email"
+  endpoint  = "satyapanuganti@gmail.com"
+}
+
+resource "aws_sns_topic_subscription" "devops-team" {
+  topic_arn = aws_sns_topic.devops.arn
+  protocol  = "email"
+  endpoint  = "satyanarayana.pr@sonata-software.com"
+}
+
+module "health_check-cloudlinuxacademy" {
+  source            = "https://github.com/Nuagic/terraform-aws-route53-health-check"
+  fqdn              = "www.cloudlinuxacadem.com"
+  port              = 80
+  type              = "HTTP"
+  resource_path     = "/"
+  failure_threshold = "5"
+  request_interval  = "30"
+  sns_topic         = "${aws_sns_topic.devops.id}"
+  name              = "APP alarm"
+}
